9021beebdaf9abc0088400ce5544358912357f06
Zota0 <kontakt.pcgamerpl6.yt@gmail.com>
2025-03-19 20:15:26 +0100

    Michal to cwl

---
+++ dashboard.php
@@ -0,0 +1,18 @@
+<?php
+if(!isset($_SESSION['login'])) {
+    header('Location: index.php');
+    exit(302);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Panel Administacyjny</title>
+</head>
+<body>
+    
+</body>
+</html>
